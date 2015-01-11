0dabbb6d4b49aa05d4dd44158ca1111616e54fc5
Ben Orchard <thefirstmuffinman@gmail.com>
2015-01-11 19:43:40 +0000

    wrapper: +autolink extension

--- redcarpet-shortlink-wrapper.rb
+++ redcarpet-shortlink-wrapper.rb
@@ -8,5 +8,6 @@ require '/home/raehik/bin/redcarpet-shortlink.rb'
 puts Redcarpet::Markdown.new(HTMLWithShortlinks, extensions = {
   no_intra_emphasis: true,
   strikethrough: true,
-  footnotes: true
+  footnotes: true,
+  autolink: true
 }).render(ARGF.read)
