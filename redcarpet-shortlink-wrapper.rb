93f3c2df13f5ea3ae870a789ef9ca093db1e702f
Ben Orchard <thefirstmuffinman@gmail.com>
2015-02-15 23:02:48 +0000

    wrapper: +toc data; +superscript

--- redcarpet-shortlink-wrapper.rb
+++ redcarpet-shortlink-wrapper.rb
@@ -5,10 +5,14 @@
 
 require '/home/raehik/bin/redcarpet-shortlink.rb'
 
-puts Redcarpet::Markdown.new(HTMLWithShortlinks, extensions = {
-  no_intra_emphasis: true,
-  strikethrough: true,
-  footnotes: true,
-  autolink: true,
-  fenced_code_blocks: true
+puts Redcarpet::Markdown.new(
+  HTMLWithShortlinks.new(
+    render_options = { with_toc_data: true}),
+  extensions = {
+    no_intra_emphasis: true,
+    strikethrough: true,
+    footnotes: true,
+    autolink: true,
+    fenced_code_blocks: true,
+    superscript: true
 }).render(ARGF.read)
